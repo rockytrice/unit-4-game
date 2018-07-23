3b513f0e5ad0df8e122e83ea879a4c310cc95d7a
Roderick Trice <Rodericktrice@Rodericks-MacBook-Pro.local>
2018-07-23 15:22:13 -0400

    added score to write to the html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,37 +12,41 @@
      var losses = 0;
      $("#losses").text(losses);
 
-
-
-
-    
-
      // target number randomly selects a number between 120 and 19
      var targetNumber = [Math.floor(Math.random() * 101) + 19];
      console.log("target number is " + targetNumber);
      // setting this h1 to match the targetNumber which will allow me to change the HTML to match the value of the targetnumber in javascript.
      $("#targetscore").text(targetNumber);
-// randomly selects numbers 1 -12 for each crystal value
-    var blueCrystalBtn = Math.floor(Math.random() * 11) + 1;
-    console.log("bluecrystal is " + blueCrystalBtn);
-    var magentaCrystalBtn = Math.floor(Math.random() *11) + 1;
-    console.log("magentacyrstal is " + magentaCrystalBtn);
+     // randomly selects numbers 1 -12 for each crystal value and logging to the console.
+     var blueCrystalBtn = Math.floor(Math.random() * 11) + 1;
+     console.log("bluecrystal value is " + blueCrystalBtn);
+     var magentaCrystalBtn = Math.floor(Math.random() * 11) + 1;
+     console.log("magentacyrstal value is " + magentaCrystalBtn);
+     var pinkCrystalBtn = Math.floor(Math.random() * 11) + 1;
+     console.log("pinkcrystal value is " + pinkCrystalBtn);
+     var purpleCrystalBtn = Math.floor(Math.random() * 11) + 1;
+     console.log("purplecrystal value " + purpleCrystalBtn);
 
 
-        
 
 
+     $("#blue-crystal").on("click", function () {
+         //     score = parseInt(score) + parseInt(blueCrystalBtn);
+         $("#userscore").html("<p>" + blueCrystalBtn + "</p>");
+         console.log("blue crystal is " + blueCrystalBtn);
 
-$(".blue-crystal").on("click", function(){
-    score = parseInt(score) + parseInt(blueCrystalBtn);
-$("#userscore").text(score);
+     });
+     $("#pink-crystal").on("click", function () {
+         $("#userscore").html("<p>" + pinkCrystalBtn + "</p>");
+         console.log("pink crystal " + pinkCrystalBtn);
+     })    
 
-console.log("user score " + score);
 
-});
-     
-     
-    
+$("#purple-crystal").on("click", function (){
+    $("#userscore").html("<h1>" + purpleCrystalBtn + "</h1>");
+    console.log("purple crystal " + purpleCrystalBtn);
+
+})
 
 
 
