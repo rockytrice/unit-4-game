a3f7480ee99222b871698831ef0b2abe2fb5f505
Roderick Trice <Rodericktrice@Rodericks-MacBook-Pro.local>
2018-07-24 00:18:27 -0400

    added comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,10 +13,6 @@ $(document).ready(function () {
     var losses = 0;
     $("#losses").text(losses);
 
-
-
-
-
     // target number randomly selects a number between 120 and 19
     var targetNumber = [Math.floor(Math.random() * 101) + 19];
     // console.log("target number is " + targetNumber);
@@ -32,32 +28,36 @@ $(document).ready(function () {
     var purpleCrystalBtn = Math.floor(Math.random() * 11) + 1;
     console.log("purplecrystal value " + purpleCrystalBtn);
 
-
+    
+// on the click of the blue crystal a value is generated and is added to the overall score
     $("#blue-crystal").on("click", function () {
 
         score = parseInt(score) + parseInt(blueCrystalBtn);
         $("#userscore").text(score);
         console.log(score);
+        // if the user score and targetnumber are equal. you win
         if (score === targetNumber) {
             wins++;
             $("#wins").text(wins);
 
             console.log("you win");
         }
+        // if the score is larger than the targetnumber you lose.
         if (score > targetNumber) {
             losses++;
             $("#losses").text(losses);
-
             console.log("you lose");
             reset();
         }
         
 
     });
-
+// same concept of the previous on click button
     $("#pink-crystal").on("click", function () {
+        //  parses a string and returns an integer
         score = parseInt(score) + parseInt(pinkCrystalBtn);
         $("#userscore").text(score);
+        // logs the score to the console
         console.log(score);
         if (parseInt(score) === parseInt(targetNumber)) {
             wins++;
@@ -66,6 +66,7 @@ $(document).ready(function () {
             console.log("you win");
             reset();
         }
+         // if the score is larger than the targetnumber you lose and the losses on the page go up by 1.
         if (score > targetNumber) {
             losses++;
             $("#losses").text(losses);
@@ -75,6 +76,7 @@ $(document).ready(function () {
 
         }
     });
+// same concept of the previous on click button
 
     $("#purple-crystal").on("click", function () {
         score = parseInt(score) + parseInt(purpleCrystalBtn);
@@ -88,6 +90,7 @@ $(document).ready(function () {
 
             console.log("you win");
         }
+         // if the score is larger than the targetnumber you lose and the losses on the page go up by 1.
         if (score > targetNumber) {
             losses++;
             $("#losses").text(losses);
@@ -100,7 +103,7 @@ $(document).ready(function () {
 
     });
 
-
+// same concept of the previous on click button
 
     $("#magenta-crystal").on("click", function () {
         score = parseInt(score) + parseInt(magentaCrystalBtn);;
@@ -110,13 +113,15 @@ $(document).ready(function () {
         if (parseInt(score) === parseInt(targetNumber)) {
             wins++;
             $("#wins").text(wins);
-
+            // logs you win to the console
             console.log("you win");
         }
+        // if the score is larger than the targetnumber you lose and the losses on the page go up by 1.
         if (score > targetNumber) {
             $("#losses").text(losses);
             losses++;
             $("#losses").text(losses);
+            // logs you lose to the console
             console.log("you lose");
             reset();
         }
@@ -126,7 +131,7 @@ $(document).ready(function () {
 
 
 
-
+//  this function resets the value of the target number, score, and the crystal values.
 
     function reset() {
      score=0;
