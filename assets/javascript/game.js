e09890b91b089002891d5806fd98a1fcc1287cf0
Roderick Trice <rocky_trice@yahoo.com>
2018-08-14 22:31:13 -0400

    fixed crystals not regenerating random number on the crystal button. was logging correctly but not on the crystal button.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,25 +13,24 @@ $(document).ready(function () {
     var losses = 0;
     $("#losses").text(losses);
 
-    // target number randomly selects a number between 120 and 19
+    // target number randomly selects a number between 12 and 19
     var targetNumber = [Math.floor(Math.random() * 101) + 19];
     // console.log("target number is " + targetNumber);
     // setting this h1 to match the targetNumber which will allow me to change the HTML to match the value of the targetnumber in javascript.
     $("#targetscore").text(targetNumber);
     // randomly selects numbers 1 -12 for each crystal value and logging to the console.
-    var blueCrystalBtn = Math.floor(Math.random() * 11) + 1;
+    var blueCrystalBtn = Math.floor(Math.random() * 12) + 1;
     console.log("bluecrystal value is " + blueCrystalBtn);
-    var magentaCrystalBtn = Math.floor(Math.random() * 11) + 1;
+    var magentaCrystalBtn = Math.floor(Math.random() * 12) + 1;
     console.log("magentacyrstal value is " + magentaCrystalBtn);
-    var pinkCrystalBtn = Math.floor(Math.random() * 11) + 1;
+    var pinkCrystalBtn = Math.floor(Math.random() * 12) + 1;
     console.log("pinkcrystal value is " + pinkCrystalBtn);
-    var purpleCrystalBtn = Math.floor(Math.random() * 11) + 1;
-    console.log("purplecrystal value " + purpleCrystalBtn);
+    var purpleCrystalBtn = Math.floor(Math.random() * 12) + 1;
+    console.log("purplecrystal value is " + purpleCrystalBtn);
 
-    
-// on the click of the blue crystal a value is generated and is added to the overall score
+
+    // on the click of the blue crystal a value is generated and is added to the overall score
     $("#blue-crystal").on("click", function () {
-
         score = parseInt(score) + parseInt(blueCrystalBtn);
         $("#userscore").text(score);
         console.log(score);
@@ -39,21 +38,23 @@ $(document).ready(function () {
         if (score === targetNumber) {
             wins++;
             $("#wins").text(wins);
-
             console.log("you win");
             reset();
+
         }
+        
+
         // if the score is larger than the targetnumber you lose.
         if (score > targetNumber) {
             losses++;
             $("#losses").text(losses);
             console.log("you lose");
             reset();
+
         }
-        
 
     });
-// same concept of the previous on click button
+    // same concept of the previous on click button
     $("#pink-crystal").on("click", function () {
         //  parses a string and returns an integer
         score = parseInt(score) + parseInt(pinkCrystalBtn);
@@ -63,21 +64,20 @@ $(document).ready(function () {
         if (parseInt(score) === parseInt(targetNumber)) {
             wins++;
             $("#wins").text(wins);
-
             console.log("you win");
             reset();
         }
-         // if the score is larger than the targetnumber you lose and the losses on the page go up by 1.
-        if (score > targetNumber) {
+        // if the score is larger than the targetnumber you lose and the losses on the page go up by 1.
+        else if (score > targetNumber) {
             losses++;
             $("#losses").text(losses);
-
             console.log("you lose");
             reset();
 
         }
+
     });
-// same concept of the previous on click button
+    // same concept of the previous on click button
 
     $("#purple-crystal").on("click", function () {
         score = parseInt(score) + parseInt(purpleCrystalBtn);
@@ -87,12 +87,12 @@ $(document).ready(function () {
         if (parseInt(score) === parseInt(targetNumber)) {
             wins++;
             $("#wins").text(wins);
-            reset();
-
             console.log("you win");
+            reset();
         }
-         // if the score is larger than the targetnumber you lose and the losses on the page go up by 1.
-        if (score > targetNumber) {
+
+        // if the score is larger than the targetnumber you lose and the losses on the page go up by 1.
+        else if (score > targetNumber) {
             losses++;
             $("#losses").text(losses);
 
@@ -104,7 +104,7 @@ $(document).ready(function () {
 
     });
 
-// same concept of the previous on click button
+    // same concept of the previous on click button
 
     $("#magenta-crystal").on("click", function () {
         score = parseInt(score) + parseInt(magentaCrystalBtn);;
@@ -116,9 +116,11 @@ $(document).ready(function () {
             $("#wins").text(wins);
             // logs you win to the console
             console.log("you win");
+            reset();
         }
+
         // if the score is larger than the targetnumber you lose and the losses on the page go up by 1.
-        if (score > targetNumber) {
+        else if (score > targetNumber) {
             $("#losses").text(losses);
             losses++;
             $("#losses").text(losses);
@@ -126,31 +128,30 @@ $(document).ready(function () {
             console.log("you lose");
             reset();
         }
-        
+
     });
 
 
 
 
-//  this function resets the value of the target number, score, and the crystal values.
+    //  this function resets the value of the target number, score, and the crystal values.
 
     function reset() {
-     score=0;
-     $("#userscore").text(score);
-     targetNumber=0;
-     targetNumber = Math.floor(Math.random() * 101) + 19;
-     $("#targetscore").text(targetNumber);
-     var blueCrystalBtn = Math.floor(Math.random() * 11) + 1;
-    console.log("new bluecrystal value is " + blueCrystalBtn);
-    var magentaCrystalBtn = Math.floor(Math.random() * 11) + 1;
-    console.log("new magentacyrstal value is " + magentaCrystalBtn);
-    var pinkCrystalBtn = Math.floor(Math.random() * 11) + 1;
-    console.log("new pinkcrystal value is " + pinkCrystalBtn);
-    var purpleCrystalBtn = Math.floor(Math.random() * 11) + 1;
-    console.log("new purplecrystal value " + purpleCrystalBtn);
-    score = parseInt(score) + parseInt(blueCrystalBtn);
-    $("#userscore").text(score);
-    console.log(score);
+        score = 0;
+        $("#userscore").text(score);
+        targetNumber = 0;
+        targetNumber = Math.floor(Math.random() * 101) + 19;
+        $("#targetscore").text(targetNumber);
+         blueCrystalBtn = Math.floor(Math.random() * 11) + 1;
+        console.log("new bluecrystal value is " + blueCrystalBtn);
+         magentaCrystalBtn = Math.floor(Math.random() * 11) + 1;
+        console.log("new magentacyrstal value is " + magentaCrystalBtn);
+         pinkCrystalBtn = Math.floor(Math.random() * 11) + 1;
+        console.log("new pinkcrystal value is " + pinkCrystalBtn);
+         purpleCrystalBtn = Math.floor(Math.random() * 11) + 1;
+        console.log("new purplecrystal value " + purpleCrystalBtn);
+        
+
     }
 
 });
